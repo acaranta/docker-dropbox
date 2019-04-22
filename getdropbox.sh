8f0b2fe27d5003bef7ca8ae337969c9e93c43bc7
acaranta <arthur@caranta.com>
2019-04-23 01:05:38 +0200

    Refactored Version storing and logging

--- getdropbox.sh
+++ getdropbox.sh
@@ -1,15 +1,28 @@
 #!/bin/bash 
+WEBVERSION=$(curl -sIkL "https://www.dropbox.com/download?plat=lnx.x86_64" |grep Location |sed 's/.*dropbox-lnx.x86_64-//g' | sed 's/.tar.gz.*//g') 
+LOCALVERSION=$(< /root/.dropbox-dist/VERSION)
 
-if [[ $(< /.dropbox-dist/VERSION) != $(curl -sIkL "https://www.dropbox.com/download?plat=lnx.x86_64" |grep Location |sed 's/.*dropbox-lnx.x86_64-//g' | sed 's/.tar.gz.*//g') ]] ; 
+echo 
+echo "| Local version : $LOCALVERSION "
+echo "|   Web Version : $WEBVERSION"
+echo
+
+if [[ $LOCALVERSION != $WEBVERSION ]] ; 
 then
-  echo Download New Version 
-  cd /
-  curl -C - -L -o /dropbox.tgz "https://www.dropbox.com/download?plat=lnx.x86_64"
+  echo
+  echo "+----------------------+"
+  echo "| Download New Version |"
+  echo "+----------------------+"
+  cd /root
+  curl -L -o /dropbox.tgz "https://www.dropbox.com/download?plat=lnx.x86_64"
   tar xfvz /dropbox.tgz 
   rm /dropbox.tgz
   curl -L -o /usr/bin/dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py"
   chmod +x /usr/bin/dropbox.py
 else
-  echo Already Up To date
+  echo
+  echo "+--------------------+"
+  echo "| Already Up To date |"
+  echo "+--------------------+"
 fi
 
