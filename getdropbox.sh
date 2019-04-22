139e61dbc3b2f6e9ebdb1fd4b70f7659bfaf706e
acaranta <arthur@caranta.com>
2019-04-23 01:26:33 +0200

    Added source for comparison function

--- getdropbox.sh
+++ getdropbox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 WEBVERSION=$(curl -sIkL "https://www.dropbox.com/download?plat=lnx.x86_64" |grep Location |sed 's/.*dropbox-lnx.x86_64-//g' | sed 's/.tar.gz.*//g') 
 LOCALVERSION=$(< /root/.dropbox-dist/VERSION)
+# Verison comparison source : https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
 vercomp () {
     if [[ $1 == $2 ]]
     then
